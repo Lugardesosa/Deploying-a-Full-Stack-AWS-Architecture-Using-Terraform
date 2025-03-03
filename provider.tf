9fcbb64f081162fbd63f0f646598bc433ebe1964
Esosa Mumen <120713430+Lugardesosa@users.noreply.github.com>
2025-03-03 16:49:33 +0100

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region              = "ap-south-1"
+  #allowed_account_ids = [434605749312] #Only when using multiple accounts in aws are in use
+  default_tags {
+    tags = {
+      environment = var.env
+      managedby   = "terraform"
+    }
+  }
+}
+terraform {
+  required_version = ">= 0.15.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.29.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.6.0"
+    }
+    template = {
+      source  = "hashicorp/template"
+      version = ">= 2.2.0"
+    }
+  }
+}
